43586116e76e51e71fea0a18fc7ae2639da6adcb
Victor Cavalcante <victor@cavalcante.net>
2012-07-27 08:47:23 -0300

    Transform it in a jQuery plugin

--- spec/gridView.spec.js
+++ spec/gridView.spec.js
@@ -28,30 +28,25 @@ describe('gridView', function(){
 
 	describe('table head', function(){
 		it('should append tableHead', function(){
-			var gridView = new GridView();
-			gridView.createGrid($('#myTable'), testObj);
-
+			$('#myTable').gridView({data:testObj});
 			expect($('#myTable')).toContain('thead');
 		});
 
 		it('should append headers containing object property names', function(){
-			var gridView = new GridView();
-			gridView.createGrid($('#myTable'), testObj);
+            $('#myTable').gridView({data:testObj});
 
 			expect($('#myTable')).toContainHtml('<thead><tr><th>firstName</th><th>lastName</th><th>email</th><th>age</th></tr></thead>');
 		});
 
 		it('should append headers containing mapped headers', function(){
-			var gridView = new GridView();
-
 			var headerMapping = {
 				'firstName': 'First Name',
 				'lastName': 'Last Name',
 				'email': 'Email',
 				'age': 'Age'
 			};
+            $('#myTable').gridView({data:testObj,headerMappings:headerMapping});
 
-			gridView.createGrid($('#myTable'), testObj, headerMapping);
 
 			expect($('#myTable')).toContainHtml('<thead><tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Age</th></tr></thead>');
 		});
@@ -59,37 +54,32 @@ describe('gridView', function(){
 
 	describe('table body', function(){
 		it('should append tableBody', function(){
-			var gridView = new GridView();
-			gridView.createGrid($('#myTable'), testObj);
+            $('#myTable').gridView({data:testObj});
 
 			expect($('#myTable')).toContain('tbody');
 		});
 
 		it('should append object data to table body with first item in array', function(){
-			var gridView = new GridView();
-			gridView.createGrid($('#myTable'), testObj);
+            $('#myTable').gridView({data:testObj});
 
 			expect($('#myTable')).toContainHtml('<tr><td>Breno</td><td>Ferreira</td><td>breno@example.com</td><td>23</td></tr>')
 		});
 
 		it('should append object data to table body with second item in array', function(){
-			var gridView = new GridView();
-			gridView.createGrid($('#myTable'), testObj);
+            $('#myTable').gridView({data:testObj});
 
 			expect($('#myTable')).toContainHtml('<tr><td>Rodrigo</td><td>Vidal</td><td>vidal@example.com</td><td>23</td></tr>')
 		});
 
 		it('should append object data to table body with thrid item in array', function(){
-			var gridView = new GridView();
-			gridView.createGrid($('#myTable'), testObj);
+            $('#myTable').gridView({data:testObj});
 
 			expect($('#myTable')).toContainHtml('<tr><td>Rodrigo</td><td>Andrade</td><td>andrade@example.com</td><td>23</td></tr>')
 		});
 	});
     describe("Render table", function(){
         beforeEach(function(){
-            var gridView = new GridView();
-            gridView.createGrid($("#myTable"), testObj);
+            $('#myTable').gridView({data:testObj});
         });
         it("should append table", function(){
             expect($("#myTable")).toContain("table");
@@ -104,14 +94,13 @@ describe('gridView', function(){
     })
     describe("Render table with attributes",function(){
         it("should apply the class attribute in a table",function(){
-            var gridView = new GridView();
-            gridView.createGrid($("#myTable"), testObj,null,{"class":"teste"});
+            $('#myTable').gridView({data:testObj,tableAttr:{"class":"teste"}});
+
             expect($("#myTable table")).toHaveClass("teste");
 
         });
         it("should apply the complex attribute in a table",function(){
-            var gridView = new GridView();
-            gridView.createGrid($("#myTable"), testObj,null,{"class":"teste", "data-cliente":"Victor"});
+            $('#myTable').gridView({data:testObj,tableAttr:{"class":"teste","data-cliente":"Victor"}});
             expect($("#myTable table")).toHaveClass("teste");
             expect($("#myTable table").data("cliente")).toBe("Victor");
         });
@@ -120,8 +109,7 @@ describe('gridView', function(){
     describe("if the container is a table",function(){
         beforeEach(function()   {
             setFixtures('<div id="myContainerTable"><table></table></div>');
-            var gridView = new GridView();
-            gridView.createGrid($("#myContainerTable table"), testObj);
+            $('#myContainerTable table').gridView({data:testObj});
         });
         it("should not render tag table ",function(){
             expect($("#myContainerTable table").length).toBe(1);
--- src/gridView.js
+++ src/gridView.js
@@ -1,59 +1,67 @@
-var GridView = function () {
-	
-};
+(function( $ ) {
+    $.fn.gridView = function(options) {
+        var settings = $.extend( {
+            'data'         : null,
+            'headerMappings' : null,
+            'tableAttr':{}
+        }, options);
 
-GridView.prototype.createGrid = function(element, data, headerMappings,tableAttr) {
-    var $table = this.getTable(element,tableAttr);
-    $table.append(this.createTableHead(element, data, headerMappings));
-    $table.append(this.createTableBody(element, data, headerMappings));
-};
+        var $table = getTable(this,settings.tableAttr);
+        $table.append(createTableHead(this, settings.data, settings.headerMappings));
+        $table.append(createTableBody(this, settings.data, settings.headerMappings));
 
-GridView.prototype.getTable = function(element,tableAttr){
-    var $table;
-    if(element.is("table")) {
-        $table = element;
-    }else{
-        element.append($("<table>",tableAttr));
-        $table = element.find("table");
-    }
-    return $table;
-}
+        function getTable(element,tableAttr){
 
-GridView.prototype.createTableHead = function(element, data, headerMappings) {
-	var thead = $('<thead></thead>');
+            var $table;
+            if(element.is("table")) {
+                $table = element;
+            }else{
+                element.append($("<table>",tableAttr));
+                $table = element.find("table");
+            }
+            return $table;
 
-	var tr = $('<tr></tr>');
-
-	var firstObject = data[0];
-	for(property in firstObject)
-	{
-		if(!headerMappings)
-			tr.append('<th>' + property + '</th>');
-		else
-			tr.append('<th>' + headerMappings[property] + '</th>');
-	}
-
-	thead.append(tr);
-
-	return thead;
-};
-
-GridView.prototype.createTableBody = function(element, data, headerMappings) {
-    var tbody = $('<tbody></tbody>');
-
-    element.append(tbody);
-
-    for(var i = 0; i < data.length; i++)
-    {
-        var tr = $('<tr></tr>');
-        var obj = data[i];
-        for(property in obj)
-        {
-            tr.append('<td>' + obj[property] + '</td>');
         }
 
-        tbody.append(tr);
-    }
+        function createTableHead(element, data, headerMappings) {
+            var thead = $('<thead></thead>');
 
-   return tbody;
-};
+            var tr = $('<tr></tr>');
+
+            var firstObject = data[0];
+            for(property in firstObject)
+            {
+                if(!headerMappings)
+                    tr.append('<th>' + property + '</th>');
+                else
+                    tr.append('<th>' + headerMappings[property] + '</th>');
+            }
+
+            thead.append(tr);
+
+            return thead;
+        };
+        function createTableBody(element, data, headerMappings) {
+            var tbody = $('<tbody></tbody>');
+
+            element.append(tbody);
+
+            for(var i = 0; i < data.length; i++)
+            {
+                var tr = $('<tr></tr>');
+                var obj = data[i];
+                for(property in obj)
+                {
+                    tr.append('<td>' + obj[property] + '</td>');
+                }
+
+                tbody.append(tr);
+            }
+
+            return tbody;
+        };
+
+    };
+
+})( jQuery );
+
